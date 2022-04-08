1f530c8bf35252458e644be3d896df29620d5d3b
Marcel M <marcel.mulders@hotmail.com>
2022-04-08 21:11:39 +0100

    Added url patterns for the new products app to point to products app

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
