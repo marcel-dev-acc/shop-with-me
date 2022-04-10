3b27b857d93d21270b9656b33d584ebeca6b3504
Marcel M <marcel.mulders@hotmail.com>
2022-04-10 13:26:42 +0100

    Added remove item from shapping bag functionality. Also added jQuery minified

--- shop/bag/urls.py
+++ shop/bag/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.ViewBag.as_view(), name="view_bag"),
     path("add/<item_id>", views.AddToBag.as_view(), name="add_to_bag"),
     path('adjust/<item_id>/', views.AdjustBag.as_view(), name='adjust_bag'),
+    path('remove/<item_id>/', views.RemoveItem.as_view(), name='remove_from_bag'),
 ]
--- shop/bag/views.py
+++ shop/bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.views import View
 from products.models import Product
@@ -98,3 +98,37 @@ class AdjustBag(View):
 
         request.session['bag'] = bag
         return redirect(reverse('view_bag'))
+
+
+class RemoveItem(View):
+    """
+    Remove the item from the shopping bag
+    """
+
+    def post(self, request, item_id):
+        """
+        Handle post request to remove item
+        """
+
+        try:
+            product = get_object_or_404(Product, pk=item_id)
+            size = None
+            if 'product_size' in request.POST:
+                size = request.POST['product_size']
+            bag = request.session.get('bag', {})
+
+            if size:
+                del bag[item_id]['items_by_size'][size]
+                if not bag[item_id]['items_by_size']:
+                    bag.pop(item_id)
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            else:
+                bag.pop(item_id)
+                messages.success(request, f'Removed {product.name} from your bag')
+
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+
+        except Exception as e:
+            messages.error(request, f'Error removing item: {e}')
+            return HttpResponse(status=500)
